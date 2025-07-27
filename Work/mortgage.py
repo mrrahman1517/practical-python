9a9d7d6fbd0dc7d6db5e563480d2c5c3aa4a981b
Muntasir Raihan Rahman <mrahman@keystonestrategy.com>
2025-07-27 11:59:21 -0700

    add mortgage hw

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,18 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+# mortgage.py
+
+principal = 815660.0
+rate = 0.05
+one_time_payments = 1000
+payment = 4206 + one_time_payments
+total_paid = 0.0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    print('current principal remaining', principal)
+
+print('Total paid', total_paid)
