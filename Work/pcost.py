34c7a287c56e4c87b45eeb3702ab184fe88e77de
Muntasir Raihan Rahman <mrahman@keystonestrategy.com>
2025-07-27 13:42:10 -0700

    solved exercises

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+    headers = next(f)
+    print(headers)
+    total_cost = 0
+    for line in f:
+        row = line.split(',')
+        print(row)
+        try:
+            shares = int(row[1])
+            price = float(row[2])
+            total_cost += shares * price
+        except ValueError:
+            print('formatting error, continuing ...')
+            continue
+    return total_cost
 
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f)
-total_cost = 0
-for line in f:
-    row = line.split(',')
-    print(row)
-    total_cost += int(row[1]) * float(row[2])
-
-print('Total cost ', total_cost)
+print('Total cost ', portfolio_cost('Data/portfolio.csv'))
